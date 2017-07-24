f85c65bfb1e146ca648a652428822f7d80ad05b8
luoyecong <luoyec@talkmoney.cn>
2017-07-24 12:11:22 +0800

    使用模板引擎

--- project_02/src/server/server.js
+++ project_02/src/server/server.js
@@ -7,54 +7,61 @@ const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
 const mongooes = require('mongoose')
+const exphbs  = require('express-handlebars')
 
+/**
+ * 端口号等配置
+ */
+const port = process.env.PORT || 3000
+const app = express()
+mongooes.Promise = global.Promise
 
-const port = process.env.PORT || 3000;
-const app = express();
+/**
+ * 模板引擎设置
+ */
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
 
-mongooes.Promise = global.Promise;
-const handlebars = require("express-handlebars").create({ // 引入模板
-	defaultLayout: "index"
-});
-
-
+/**
+ * 数据库连接
+ */
 mongooes.connection.openUri('mongodb://localhost:27017/movie')
 const connect = mongooes.connection
 connect.on('error', console.error.bind(console, '连接错误: '))
 connect.once('open', () => { console.log('数据库成功连接') })
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+/**
+ * 使用body中间件解析post请求
+ */
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
-//引入路由
+//引入后端路由
 const loginRouter = require('./routes')
 
-app.engine("handlebars", handlebars.engine);
-app.set("view engine", "handlebars");
+// app.engine("handlebars", handlebars.engine);
+// app.set("view engine", "handlebars");
 
-app.use(express.static("dist")); // 托管静态文件
-app.use(body_parser); // body中间件解析post请求
-
+app.use(express.static("build")) // 托管静态文件
 
 //将前端路由请求/;跳转到index
 app.get('/', (req, res) => {
-    //res.send('Hello World!')
-    res.render('index')
+    res.render('home')
 })
 
 //app.use('/login',loginRouter)
 
-app.get('/login', (req, res) => {
-    let userData = {
-        loginName: req.body.loginName,
-        passWord: req.body.passWord
-    }
-    res.json({
-        code: '200',
-        status: 'success',
-        msg: '登陆成功'
-    })
-})
+// app.get('/login', (req, res) => {
+//     let userData = {
+//         loginName: req.body.loginName,
+//         passWord: req.body.passWord
+//     }
+//     res.json({
+//         code: '200',
+//         status: 'success',
+//         msg: '登陆成功'
+//     })
+// })
 
 app.listen(port, () => {
     console.log('开始监听：' + port)
